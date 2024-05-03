9e02943cebcec4c3d52a45589e3e7a1e843c085e
Simply-Box <103854507+Simply-Box@users.noreply.github.com>
2024-05-03 17:41:24 +0200

    Changed names and comments

--- PrimesGen.py
+++ PrimesGen.py
@@ -1,24 +1,23 @@
-# This code generates primes up to the scope nummber and currently prints the nummber of primes
-
+# This code generates primes up to the scope nummber (all prime nummbers below the scope) and currently prints the nummber of primes
 import numpy as np
 
-# Variabler
-antalP = 0
+nummberOfPrime = 0
 scope =  100 #100_000_000
 
 # True == Prime, False == No Prime
-lis = np.full(scope, True)
-lis[0] = False
-lis[1] = False
+list = np.full(scope, True)
+# 0 and 1 are not prime
+list[0] = False
+list[1] = False
 
 for i in range(2, scope):
-    # If prime: Make multiples of i False
-    if lis[i]:
-        antalP+=1
-        # Uncomment code if you want to find the 10001:st prime nummber
-        #if antal == 10001:
-        #    print(i)
+    # If i prime: Make multiples of i False
+    if list[i]:
         for j in range(i*2, scope, i):
-            lis[j] = False
+            list[j] = False
+        nummberOfPrime+=1
+        # Uncomment code if you want to for example find the 10001:st prime nummber, print all primes and so on
+        #if nummberOfPrime == 10001:
+        #    print(i)
 
 print(antalP)
